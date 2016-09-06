8595845bfb00e034c62bdec7346018cae6784608
Matt Carroll <mattcarroll@Matts-MacBook-Pro.local>
2016-09-06 10:29:21 +1000

    delete bm-identity.js

--- packages/bm-identity.js/lib/constants.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const constants = {
-  AUTH0_URL: 'https://blinkmobile.auth0.com',
-  AUTH0_CALLBACK_URL: 'https://auth.blinkm.io/index.html'
-};
-
-module.exports = Object.freeze(constants);
